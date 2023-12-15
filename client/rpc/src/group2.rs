1210840d651329de418878e70200c3a366ab6b08
Alexander Gryaznov <hi@agryaznov.com>
2023-12-15 20:55:26 +0200

    get_block_by_hash()

--- client/rpc/src/group2.rs
+++ client/rpc/src/group2.rs
@@ -7,8 +7,16 @@ where
     C: ProvideRuntimeApi<B> + HeaderBackend<B> + BlockBackend<B> + 'static,
     C::Api: ETHRuntimeRPC<B>,
 {
-    pub async fn block_by_hash(&self, _hash: H256, _full: bool) -> RpcResult<Option<RichBlock>> {
-        Ok(None)
+    pub async fn block_by_hash(&self, hash: H256, _full: bool) -> RpcResult<Option<RichBlock>> {
+        Ok(self
+            .client
+            .block(hash)
+            .map_err(|err| internal_err(format!("Failed fetching block: {:?}", err)))?
+            .map(|b| EthBlock::from(SubBlock(b.block)))
+            .map(|b| RichBlock {
+                inner: b,
+                extra_info: BTreeMap::new(),
+            }))
     }
 
     pub async fn block_by_number(
@@ -34,17 +42,16 @@ where
             BlockNumber::Num(num) => {
                 // block num in substrate db is u32
                 // https://github.com/paritytech/polkadot-sdk/blob/73c2bca9cdb17f1fdc2afd7aed826d0c55b8640a/substrate/client/rpc/src/chain/mod.rs#L75
-                let number = <NumberFor<B>>::try_from(num).map_err(|err| {
-                    internal_err(format!("Error converting block number: {:?}", num))
-                })?;
+                let number = <NumberFor<B>>::try_from(num)
+                    .map_err(|err| internal_err(format!("Error converting block: {:?}", num)))?;
                 self.client
                     .hash(number)
-                    .map_err(|err| internal_err(format!("Failed fetching block hash: {:?}", err)))?
+                    .map_err(|err| internal_err(format!("Failed fetching block: {:?}", err)))?
             }
             BlockNumber::Earliest => self
                 .client
                 .hash(0u32.into())
-                .map_err(|err| internal_err(format!("Failed fetching block hash: {:?}", err)))?,
+                .map_err(|err| internal_err(format!("Failed fetching block: {:?}", err)))?,
             BlockNumber::Latest | BlockNumber::Pending => Some(self.client.info().best_hash),
             BlockNumber::Safe | BlockNumber::Finalized => Some(self.client.info().finalized_hash),
             BlockNumber::Hash { hash, .. } => Some(hash.into()),
--- client/rpc/src/group3.rs
+++ client/rpc/src/group3.rs
@@ -25,6 +25,23 @@ where
     }
 
     pub async fn transaction_receipt(&self, _hash: H256) -> RpcResult<Option<Receipt>> {
+        // Ok(Some(Receipt {
+        //      transaction_hash: Option<H256>,
+        //      transaction_index: Option<U256>,
+        //      block_hash: Option<H256>,
+        //      from: Option<H160>,
+        //      to: Option<H160>,
+        //      block_number: Option<U256>,
+        //      contract_address: Default::default(),
+        //      cumulative_gas_used: Default::default(),
+        //      gas_used: Option<U256>,
+        //      logs: Vec<Log>,
+        //      state_root: Option<H256>,
+        //      logs_bloom: H2048,
+        //      status_code: Option<U64>,
+        //      effective_gas_price: U256,
+        //      transaction_type: U256,
+        // }))
         Ok(None)
     }
 }
