4f05aea43b0e58588d547c1c8b45847aad377089
Alexander Gryaznov <hi@agryaznov.com>
2023-12-15 21:33:23 +0200

    +block_transaction_count*()

--- client/rpc/src/group2.rs
+++ client/rpc/src/group2.rs
@@ -71,14 +71,42 @@ where
             }))
     }
 
-    pub async fn block_transaction_count_by_hash(&self, _hash: H256) -> RpcResult<Option<U256>> {
-        Ok(None)
+    pub async fn block_transaction_count_by_hash(&self, hash: H256) -> RpcResult<Option<U256>> {
+        Ok(self
+            .client
+            .block(hash)
+            .map_err(|err| internal_err(format!("Failed fetching block body: {:?}", err)))?
+            .map(|b| b.block.extrinsics().iter().count().into()))
     }
 
     pub async fn block_transaction_count_by_number(
         &self,
-        _number: BlockNumber,
+        number: BlockNumber,
     ) -> RpcResult<Option<U256>> {
-        Ok(None)
+        // TODO: put to utils?
+        let hash = match number {
+            BlockNumber::Num(num) => {
+                // block num in substrate db is u32
+                // https://github.com/paritytech/polkadot-sdk/blob/73c2bca9cdb17f1fdc2afd7aed826d0c55b8640a/substrate/client/rpc/src/chain/mod.rs#L75
+                let number = <NumberFor<B>>::try_from(num)
+                    .map_err(|err| internal_err(format!("Error converting block: {:?}", num)))?;
+                self.client
+                    .hash(number)
+                    .map_err(|err| internal_err(format!("Failed fetching block: {:?}", err)))?
+            }
+            BlockNumber::Earliest => self
+                .client
+                .hash(0u32.into())
+                .map_err(|err| internal_err(format!("Failed fetching block: {:?}", err)))?,
+            BlockNumber::Latest | BlockNumber::Pending => Some(self.client.info().best_hash),
+            BlockNumber::Safe | BlockNumber::Finalized => Some(self.client.info().finalized_hash),
+            BlockNumber::Hash { hash, .. } => Some(hash.into()),
+        }
+        .unwrap_or(self.client.info().best_hash);
+        Ok(self
+            .client
+            .block(hash)
+            .map_err(|err| internal_err(format!("Failed fetching block body: {:?}", err)))?
+            .map(|b| b.block.extrinsics().iter().count().into()))
     }
 }
