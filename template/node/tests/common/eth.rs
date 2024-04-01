28769c1bc5e97abd8064c6f25b33ebd1213445ac
Alexander Gryaznov <hi@agryaznov.com>
2024-04-01 15:46:36 +0300

    e2e: ease Eth tx composing with EthTxInput

--- template/node/tests/common/eth.rs
+++ template/node/tests/common/eth.rs
@@ -1,44 +1,76 @@
 use crate::{
-    ecdsa, EthTransaction, EthereumSignature, LegacyTransaction, LegacyTransactionMessage,
+    ecdsa, Bytes, EthTransaction, EthereumSignature, LegacyTransaction, LegacyTransactionMessage,
     SubstrateWeight, TransactionSignature, U256,
 };
 
-pub fn compose_and_sign_eth_tx(
-    nonce: u64,
-    gas_price: u64,
-    gas_limit: SubstrateWeight,
+#[derive(Clone)]
+pub struct EthTxInput {
+    nonce: U256,
+    gas_price: U256,
+    gas_limit: U256,
     action: ethereum::TransactionAction,
-    value: u64,
+    value: U256,
     input: Vec<u8>,
     chain_id: Option<u64>,
     signer: ecdsa::Pair,
-) -> EthTransaction {
-    let nonce = nonce.into();
-    let gas_price = gas_price.into();
-    let gas_limit: U256 = gas_limit.into();
-    let value = value.into();
+}
 
-    let msg = LegacyTransactionMessage {
-        nonce,
-        gas_price,
-        gas_limit: gas_limit.clone(),
-        action,
-        value,
-        input: input.clone(),
-        chain_id,
-    };
+impl EthTxInput {
+    pub fn new(
+        nonce: u64,
+        gas_price: u64,
+        gas_limit: SubstrateWeight,
+        action: ethereum::TransactionAction,
+        value: u64,
+        input: Vec<u8>,
+        chain_id: Option<u64>,
+        signer: ecdsa::Pair,
+    ) -> Self {
+        let nonce = nonce.into();
+        let gas_price = gas_price.into();
+        let gas_limit: U256 = gas_limit.into();
+        let value = value.into();
 
-    let sig = EthereumSignature::new(signer.sign_prehashed(&msg.hash().into()));
+        Self {
+            nonce,
+            gas_price,
+            gas_limit,
+            action,
+            value,
+            input,
+            chain_id,
+            signer,
+        }
+    }
+}
+
+impl From<EthTxInput> for LegacyTransactionMessage {
+    fn from(v: EthTxInput) -> Self {
+        Self {
+            nonce: v.nonce,
+            gas_price: v.gas_price,
+            gas_limit: v.gas_limit,
+            action: v.action,
+            value: v.value,
+            input: v.input,
+            chain_id: v.chain_id,
+        }
+    }
+}
+
+pub fn compose_and_sign_eth_tx(i: EthTxInput) -> EthTransaction {
+    let msg: LegacyTransactionMessage = i.clone().into();
+    let sig = EthereumSignature::new(i.signer.sign_prehashed(&msg.hash().into()));
     let sig: Option<TransactionSignature> = sig.into();
     let signature = sig.expect("signer generated no signature");
 
     EthTransaction::Legacy(LegacyTransaction {
-        nonce,
-        gas_price,
-        gas_limit,
-        action,
-        value,
-        input,
+        nonce: i.nonce,
+        gas_price: i.gas_price,
+        gas_limit: i.gas_limit,
+        action: i.action,
+        value: i.value,
+        input: i.input,
         signature,
     })
 }
