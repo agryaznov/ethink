fecfbe678a56ec29112bc86e680712cfe5eb5f24
Alexander Gryaznov <hi@agryaznov.com>
2024-04-02 17:07:41 +0300

    cleanup ep-rpc

--- primitives/rpc/src/lib.rs
+++ primitives/rpc/src/lib.rs
@@ -16,111 +16,24 @@
 #![allow(clippy::too_many_arguments)]
 #![deny(unused_crate_dependencies)]
 
-use ethereum::Log;
 pub use ethereum::TransactionV2 as EthTransaction;
-use ethereum_types::Bloom;
-use scale_codec::{Decode, Encode};
-use scale_info::TypeInfo;
-
-pub use evm::{backend::Basic as EthAccount, ExitReason};
 // Substrate
-use sp_core::{Hasher, H160, H256, U256};
-use sp_runtime::{traits::Block as BlockT, RuntimeDebug};
-use sp_state_machine::OverlayedChanges;
+use sp_core::{H160, U256};
+use sp_runtime::traits::Block as BlockT;
 use sp_std::vec::Vec;
-
-#[derive(Clone, Eq, PartialEq, Default, RuntimeDebug, Encode, Decode, TypeInfo)]
-pub struct TransactionStatus {
-    pub transaction_hash: H256,
-    pub transaction_index: u32,
-    pub from: H160,
-    pub to: Option<H160>,
-    pub contract_address: Option<H160>,
-    pub logs: Vec<Log>,
-    pub logs_bloom: Bloom,
-}
-
-#[derive(Eq, PartialEq, Clone, Encode, Decode, sp_runtime::RuntimeDebug)]
-pub struct TxPoolResponse {
-    pub ready: Vec<ethereum::TransactionV2>,
-    pub future: Vec<ethereum::TransactionV2>,
-}
-
-pub trait RuntimeStorageOverride<B: BlockT, C, H: Hasher>: Send + Sync {
-    fn is_enabled() -> bool;
-
-    fn set_overlayed_changes(
-        client: &C,
-        overlayed_changes: &mut OverlayedChanges<H>,
-        block: B::Hash,
-        version: u32,
-        address: H160,
-        balance: Option<U256>,
-        nonce: Option<U256>,
-    );
-
-    fn into_account_id_bytes(address: H160) -> Vec<u8>;
-}
-
-impl<B: BlockT, C, H: Hasher> RuntimeStorageOverride<B, C, H> for () {
-    fn is_enabled() -> bool {
-        false
-    }
-
-    fn set_overlayed_changes(
-        _client: &C,
-        _overlayed_changes: &mut OverlayedChanges<H>,
-        _block: B::Hash,
-        _version: u32,
-        _address: H160,
-        _balance: Option<U256>,
-        _nonce: Option<U256>,
-    ) {
-    }
-
-    fn into_account_id_bytes(_address: H160) -> Vec<u8> {
-        Vec::default()
-    }
-}
-
-#[derive(Clone, Copy, Eq, PartialEq, Debug, Encode, Decode, TypeInfo)]
-#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
-pub struct WeightInfo {
-    pub ref_time_limit: Option<u64>,
-    pub proof_size_limit: Option<u64>,
-    pub ref_time_usage: Option<u64>,
-    pub proof_size_usage: Option<u64>,
-}
-
-#[derive(Clone, Eq, PartialEq, Debug, Encode, Decode, TypeInfo)]
-#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
-pub struct UsedGas {
-    /// The used_gas as returned by the evm gasometer on exit.
-    pub standard: U256,
-    /// The result of applying a gas ratio to the most used
-    /// external metric during the evm execution.
-    pub effective: U256,
-}
-
-#[derive(Clone, Eq, PartialEq, Debug, Encode, Decode, TypeInfo)]
-#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
-pub struct EVMExecInfo<T> {
-    pub exit_reason: ExitReason,
-    pub value: T,
-    pub used_gas: UsedGas,
-    pub weight_info: Option<WeightInfo>,
-    pub logs: Vec<Log>,
-}
 
 sp_api::decl_runtime_apis! {
     /// Runtime-exposed API necessary for ETH-compatibility layer.
     pub trait ETHRuntimeRPC {
         /// Return runtime-defined CHAIN_ID.
         fn chain_id() -> u64;
+
         /// Return account balance.
         fn account_free_balance(address: H160) -> U256;
+
         /// Return account nonce.
         fn nonce(address: H160) -> U256;
+
         /// Call contract (without extrinsic submission)
         fn call(
             from: H160,
@@ -129,6 +42,7 @@ sp_api::decl_runtime_apis! {
             value: U256,
             gas_limit: U256,
         ) -> Result<Vec<u8>, sp_runtime::DispatchError>;
+
         /// Estimate gas needed for a contract call
         fn gas_estimate(
             from: H160,
@@ -137,6 +51,7 @@ sp_api::decl_runtime_apis! {
             value: U256,
             gas_limit: U256,
         ) -> Result<U256, sp_runtime::DispatchError>;
+
         /// Wrap Ethereum transaction into an extrinsic
         fn build_extrinsic(from: ethereum::TransactionV2) -> <Block as BlockT>::Extrinsic;
     }
