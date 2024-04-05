1a5cf5f80471455a5998a46c8e23970f680f3092
Alexander Gryaznov <hi@agryaznov.com>
2024-04-05 21:52:50 +0300

    Fix licenses

--- client/rpc/src/lib.rs
+++ client/rpc/src/lib.rs
@@ -1,3 +1,24 @@
+// SPDX-License-Identifier: GPL-3.0-or-later WITH Classpath-exception-2.0
+//
+// This file was derived from Frontier (fc-rpc),
+// and modified to become part of Ethink.
+//
+// Copyright (c) (Frontier): 2020-2022 Parity Technologies (UK) Ltd.
+// Copyright (c) (Ethink):   2023-2024 Alexander Gryaznov.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
 // ETH RPC methods grouped according to mapping table
 mod group1;
 mod group2;
