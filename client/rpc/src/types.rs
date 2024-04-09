2f1432a3f59b32edb3043feccae8f03f09f1f07c
Alexander Gryaznov <hi@agryaznov.com>
2024-04-09 22:15:06 +0300

    custom types module for rpc

--- client/rpc/src/types.rs
+++ client/rpc/src/types.rs
@@ -1,16 +1,47 @@
-//! Means for signing an Ethereum transaction
+//! We keep custom types here and not in rpc-core,
+//! for we might later switch to fc-rpc-core.
 use ethereum::{LegacyTransactionMessage, TransactionSignature};
 
 // Substrate
 use sp_core::ecdsa;
 use sp_keystore::KeystorePtr;
+use sp_runtime::traits::{Block as BlockT, Header, UniqueSaturatedInto};
 
 use crate::ETHINK_KEYTYPE_ID;
 use ep_crypto::{AccountId20, EthereumSignature};
+use ethereum_types::{H256, U256};
+use ethink_rpc_core::types::Header as EthHeader;
 
-// TODO move to ep_crypto
-/// ETH transaction signer, comprised of account_id and ref to a keystore holding
-/// secret key for that account_id.
+pub use ethink_rpc_core::types::Block as EthereumBlock;
+
+/// Substrate block, convertible to Ethereum block
+pub struct SubstrateBlock<B>(pub B);
+
+impl<B: BlockT<Hash = H256>> From<SubstrateBlock<B>> for EthereumBlock {
+    // Generate dumb ETH block with empty tx list,
+    // from the given substrate block
+    fn from(b: SubstrateBlock<B>) -> Self {
+        let h = b.0.header();
+
+        let header = EthHeader {
+            hash: (h.hash()).into(),
+            parent_hash: (*h.parent_hash()).into(),
+            state_root: (*h.state_root()).into(),
+            transactions_root: (*h.extrinsics_root()).into(),
+            number: Some(U256::from(
+                UniqueSaturatedInto::<u128>::unique_saturated_into(*h.number()),
+            )),
+            ..Default::default()
+        };
+
+        Self {
+            header,
+            ..Default::default()
+        }
+    }
+}
+
+/// Ethereum transaction signer with keypair stored in node's keystore
 pub struct EthereumSigner {
     keystore: KeystorePtr,
     pub_key: ecdsa::Public,
